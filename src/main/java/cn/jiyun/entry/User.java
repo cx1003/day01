9020f525666f035c02cad3a8b027ca17cbfab8ec
wang145225 <62053194+wang145225@users.noreply.github.com>
2020-04-22 10:48:19 +0800

    添加属性

--- src/main/java/cn/jiyun/entry/User.java
+++ src/main/java/cn/jiyun/entry/User.java
@@ -5,6 +5,7 @@ public class User {
     private String password;
     private String aaa1;
     private String aaa;
+    private String aaa3;
 
     public String getUsername() {
         return username;
